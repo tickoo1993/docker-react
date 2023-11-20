3cf3940a9784f6c3b5c5719647f0f7c1fdcd9aa0
tickoo1993 <kartik.tickoo@gmail.com>
2023-11-20 14:23:32 -0500

    text refactor

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hiloo
+          Edit <code>src/App.js</code> hello
         </p>
         <a
           className="App-link"
